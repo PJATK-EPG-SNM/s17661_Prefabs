9fa19b0de99ef0016c455c47bae441b03c44cd4e
danieljozwiak17661 <57590934+danieljozwiak17661@users.noreply.github.com>
2020-04-27 20:38:09 +0200

    dodanie assetów które wcześnij gdzieś zniknęły

---
+++ Assets/Scripts/Cannonball.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cannonball : MonoBehaviour
+{
+    public float speed = 5;
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+
+    void Update()
+    {
+        rb.velocity = transform.right * speed;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+
+    private void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+}
